c3b92e8af1a9d92e5fd07896cef06ef6f13d6878
olive <olive@DESKTOP-5697OEV>
2024-04-29 12:33:34 +0200

    push

--- js/rayShader.js
+++ js/rayShader.js
@@ -1,21 +1,34 @@
 class rayShader extends Shader {
-    constructor(volume, iso){
+    constructor(volume, iso) {
         super("ray_vert", "ray_frag");
+        this.initVolume(volume);
+        this.initIso(iso);
+        this.initColor();
+        this.initScale(volume);
+    }
 
+    initVolume(volume) {
         let volumeTexture = new THREE.Data3DTexture(volume.voxels, volume.width, volume.height, volume.depth);
         volumeTexture.format = THREE.RedFormat;
         volumeTexture.type = THREE.FloatType;
         volumeTexture.minFilter = volumeTexture.magFilter = THREE.LinearFilter;
         volumeTexture.unpackAlignment = 1;
         volumeTexture.needsUpdate = true;
-
         this.setUniform("volume", volumeTexture);
+    }
+
+    initIso(iso) {
         this.setUniform("iso", iso);
+    }
+
+    initColor() {
         let hexColor = document.getElementById("myColor").value;
         let execArray = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColor);
         let rgbColor = [parseInt(execArray[1], 16), parseInt(execArray[2], 16), parseInt(execArray[3], 16)];
-
         this.setUniform("color", new THREE.Vector3(rgbColor[0], rgbColor[1], rgbColor[2]), "v3v");
+    }
+
+    initScale(volume) {
         this.setUniform("scale", new THREE.Vector3(volume.width, volume.height, volume.depth), "v3v");
     }
 }
