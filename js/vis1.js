b0fe5fe7506e0c0056e753d4b4fec394974286df
olive <olive@DESKTOP-5697OEV>
2024-04-30 14:42:51 +0200

    bitte divs anpassen

--- js/vis1.js
+++ js/vis1.js
@@ -22,13 +22,12 @@ let volume = null;
 let fileInput = null;
 let myShader = null;
 let isoValue = 0.3;
-let histogramData = null;
 
-let svg, xAxis, yAxis, xLabel, yLabel, xScale, yScale, markedLine, myCircle, bars = null;
+let svg, x, y = null;
 
-let margin = 75;
-let editorWidth = 600;
-let editorHeight = 950;
+let margin = 50;
+let histogramWidth = 500;
+let histogramHeight = 800;
 
 // Initialize the application
 function init() {
@@ -53,9 +52,7 @@ function readFile() {
         console.log("Data loaded.");
         let data = new Uint16Array(reader.result);
         volume = new Volume(data);
-
-        updateHistogram(data);
-
+        
         resetVis();
     };
     reader.readAsArrayBuffer(fileInput.files[0]);
@@ -72,6 +69,11 @@ async function resetVis() {
     const boundingBox = new THREE.Mesh(geometry, myMaterial);
     scene.add(boundingBox);
     orbitCamera = new OrbitCamera(camera, new THREE.Vector3(0, 0, 0), 2 * volume.max, renderer.domElement);
+
+    svg.selectAll("rect").remove();
+
+    updateHistogram(volume.voxels);
+
     requestAnimationFrame(paint);
 }
 
@@ -90,77 +92,91 @@ function updateShaderIso(iso) {
 
 function createHistogram() {
     svg = d3.select("#histogramContainer")
-            .append("svg")
-            .attr("width", editorWidth)
-            .attr("height", editorHeight)
-            .append("g")
-            .attr("transform", 
-                  "translate(" + margin + "," + 10 + ")");
-    
-    yAxis = svg.append("g");
+        .append("svg")
+        .attr("width", histogramWidth)
+        .attr("height", histogramHeight);
 
-    xScale = d3.scaleLinear().domain([0, 1]).range([0, editorWidth / 2]);
+    x = d3.scaleLinear()
+        .domain([0.0, 1.0])
+        .range([margin, histogramWidth - margin]);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${histogramHeight / 2 - margin})`)
+        .attr("id", "xAxis")
+        .call(d3.axisBottom(x));
+
+    svg.append("text")
+        .style("fill", "white")
+        .text("density")
+        .attr("x", histogramWidth - (margin * 2))
+        .attr("y", histogramHeight / 2 - 10);
+
+    y = d3.scaleLinear()
+        .domain([0.0, 1.0])
+        .range([histogramHeight / 2 - margin, margin]);
+
+    svg.append("g")
+        .attr("transform", `translate(${margin}, 0)`)
+        .attr("id", "yAxis")
+        .call(d3.axisLeft(y));
+
+    svg.append("text")
+        .style("fill", "white")
+        .text("intensity")
+        .attr("transform", `translate(10, ${margin * 2})rotate(270)`);
 }
 
 function updateHistogram(data) {
+    const histogram = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(100));
 
-    svg.selectAll("*").remove();
+    const bins = histogram(data);
 
-    let floatD = [];
-    data.slice(3).forEach(function(voxel) {
-        if(voxel > 0) {
-            floatD.push(voxel / 4095.0);
-        }
-    });
-
-    let j = floatD.length;
-
-    let bins = d3.bin().domain(xScale.domain()).thresholds(xScale.ticks(100))(floatD);
-
-    yScale = d3.scaleLinear().domain([0, d3.max(bins, function (d) {
-        return d.length / j;
-    })])
-    .range([editorHeight / 4, 0]);
-
-    bars = svg.selectAll("rect").data(bins);
+    const yHist = d3.scalePow()
+        .range([histogramHeight / 2, 0])
+        .domain([0, d3.max(bins, function (d) {
+            return d.length;
+        })])
+        .exponent(0.2);
 
     svg.selectAll("rect")
         .data(bins)
-        .join(enter => enter.append("rect")
-                            .attr("x", 1)
-                            .attr("transform", d => "translate(" + xScale(d.x0) + "," + editorHeight / 4 + ")")
-                            .attr("width", d => xScale(d.x1) - xScale(d.x0) - 1)
-                            .attr("height", 0)
-                            .style("fill", "#AAAAAA")
-                            .call(enter => enter.transition().duration(1000)
-                            .attr("height", d => editorHeight / 4 - yScale(d.length / j))),
-                        update => update
-                            .call(update => update.transition().duration(1000)
-                            .attr("x", 1)
-                            .attr("transform", d => "translate(" + xScale(d.x0) + "," + editorHeight / 4 + ")")
-                            .attr("width", d => xScale(d.x1) - xScale(d.x0) - 1)
-                            .attr("height", d => editorHeight / 4 - yScale(d.length / n))),
-                        exit => exit
-                            .call(exit => exit.transition().duration(1000)
-                            .attr("height", 0)
-                            .remove())
-            );
-    xAxis = svg.append("g")
-        .attr("transform", "translate(0," + editorHeight / 4 + ")")
-        .call(d3.axisBottom(xScale));
-    
-        yAxis = svg.append("g")
-        .call(d3.axisLeft(yScale));
-    
-    xLabel = svg.append("text")
-        .attr("transform", "translate(" + (editorWidth + 50 / 2) + ", " + (editorHeight / 4) + ")")
-        .attr("fill", "#fff")
-        .style("text-anchor", "middle")
-        .text("Density");
-    
-    yLabel = svg.append("text")
-        .attr("transform", "translate(-45, 30) rotate(-90)")
-        .attr("fill", "#fff")
-        .style("text-anchor", "middle")
-        .text("Intensity");
+        .transition()
+        .duration(500)
+        .attr("height", function (d) {
+            return (histogramHeight / 2) - yHist(d.length);
+        });
+
+    svg.selectAll("rect")
+        .data(bins)
+        .enter()
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", margin)
+        .attr("opacity", 0.6)
+        .attr("transform", function (d) {
+            return "translate(" + x(d.x0) + "," + 300 + ")";
+        })
+        .attr("width", function (d) {
+            if ((x(d.x1) - x(d.x0)) === 0) {
+                return 0;
+            }
+            return (x(d.x1) - x(d.x0)) - 1;
+        })
+        .attr("height", 0)
+        .transition()
+        .duration(500)
+        .attr("height", function (d) {
+            return (histogramHeight / 2) - yHist(d.length);
+        })
+        .style("fill", "#03AC13");
+
+    svg.selectAll("rect")
+        .data(bins)
+        .exit()
+        .transition()
+        .duration(500)
+        .attr("height", 0)
+        .remove();
 }
